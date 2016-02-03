14358e287d0c58b37d947d5a9a3268ff32c2a38a
Azat Mardan <hi@azat.co>
2016-02-03 08:41:34 -0800

    add changes :factory:

--- test/index.js
+++ test/index.js
@@ -2,17 +2,18 @@ var test = require('tape'),
   cp = require('child_process')
 
 test('node version', function (t) {
-  t.plan(3)
-  t.equal(process.versions.node, '5.1.0')
+  t.plan(4)
   child = cp.exec('node main',
   function (error, stdout, stderr) {
-    console.log('stdout: ' + stdout)
-    console.log('stderr: ' + stderr)
-    t.equal(stderr, '')
+    // console.log('stdout: ' + stdout)
+    // console.log('stderr: ' + stderr)
+    t.equal(stderr, '', 'No errors')
     if (error !== null) {
       console.log('exec error: ' + error)
     }
     stdout = stdout.replace('\n','')
-    t.equal(stdout.indexOf('\x1b[32m')>-1, true)
+    t.true(stdout.indexOf('\x1b[32m')>-1, 'Green color present')
+    t.true(stdout.indexOf('\033[31m')>-1, 'Red color present')
+    t.false(stdout.indexOf('\x1b[33m')>-1, 'Yellow color absent')
   })
 })

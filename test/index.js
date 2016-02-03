34336deb070c76663e89f91db8ecaaaff9511654
Azat Mardan <hi@azat.co>
2016-02-03 08:38:34 -0800

    add changes :shoe:

--- test/index.js
+++ test/index.js
@@ -2,7 +2,7 @@ var test = require('tape'),
   cp = require('child_process')
 
 test('node version', function (t) {
-  t.plan(2)
+  t.plan(3)
   t.equal(process.versions.node, '5.1.0')
   child = cp.exec('node main',
   function (error, stdout, stderr) {
@@ -13,6 +13,6 @@ test('node version', function (t) {
       console.log('exec error: ' + error)
     }
     stdout = stdout.replace('\n','')
-    // t.equal(stdout, '3.3.12')
+    t.equal(stdout.indexOf('\x1b[32m')>-1, true)
   })
 })
